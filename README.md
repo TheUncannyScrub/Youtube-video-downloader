50f12de9f02f318bfd7b0261fa5d8cbf710d8b29
Unknown <scotty.dot@live.co.uk>
2018-06-14 23:27:01 +0100

    Updated files
    Updated app.js to now include download from list to audio or video and to have the oiption to not use ffmpeg with audio conversion

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-Youtube video downloader
+# Youtube video downloader
+### A Youtube video dowloader in NodeJS.
+#### With the ability to extract the audio using FFmpeg
+
+---
+**Basic Usage**:
+The app defaults to converting it to an audio file. To Download *JUST* the video add a `-v` argument to the command line.
+
+The app does not take a standard URL as a download link. Go to the search bar in your browser and copy the part of the URL **AFTER** :```https://www.youtube.com/watch?v=```
+```javascript
+node app.js dQw4w9WgXcQ -v
+```
+Just Video
+
+
+```javascript
+node app.js dQw4w9WgXcQ 
+```
+Video and Audio
+
+
+The JavaScript file *app.js* checks if two folders exist if not it creates them when you first run the file: *./music* and *./video* . The music folder stores the audio file and the Video folder stores the mp4 file. 
+
+
+This app was a 20 minute bodge because all the other version looked unsafe or dodgey. Feel free to use this even though its buggy and dont be afraid to mess on with it. Im working to make it a more inclusive app with the ability to download entire playlists at a time.
--- app.js
+++ app.js
@@ -1,50 +1,176 @@
 const fs = require('fs');
 const ytdl = require('ytdl-core');
-const ffmpeg = require('ffmpeg');
+const ffmpeg = require('fluent-ffmpeg');
 const argv = require('minimist')(process.argv.slice(2))
 
+/**
+ *  Example input
+ *  $ node index.js list.txt -l -a = Download to audio files from the list
+ * 
+ *  
+ *  -a = Audio download
+ *  -v = Video download
+ *  -d = Debug mode (Adds extra console output to view what the Code is doing)
+ *  -l = List Download (If you have a list of URLs to download)
+ *  -f = Download Audio without FFMPEG (Only available in conjunction with -a and will create massive mp3 files)
+ * 
+ * 
+ * 
+ *  Combined Flag output examples:
+ *  -a & -l = Take the list and download audio
+ *  -v & -l = Take the list and download video
+ *  -a = Audio only from Argument
+ *  -v = Video only from Argument
+ * 
+ * 
+ */
 
-let URL = `https://www.youtube.com/watch?v=${argv['_']}`;
-console.log(URL)
-let vidTitle;
-ytdl.getInfo(URL ,function(err, info) {
-    vidTitle = info.title;
-});
+
+let videoDir = './videos'  //Variable with the folder name to store videos
+if(!fs.existsSync(videoDir)){ //Check if the Video folder already exists
+    fs.mkdirSync(videoDir)}; // If not make it
+
+let audioDir = './audio' //Variable with the folder name to store audio
+if(!fs.existsSync(audioDir)){ //Check if the Audio folder already exists
+    fs.mkdirSync(audioDir)} //If not make it
+
+let tempDir = './temp' //Variable with the folder name to store temp files
+if(!fs.existsSync(tempDir)){ //Check if the Temp folder already exists
+    fs.mkdirSync(tempDir)} //if not make it
 
 
-let videoDir = './videos'
-if(!fs.existsSync(videoDir)){
-    fs.mkdirSync(videoDir)};
-let musicDir = './music'
-if(!fs.existsSync(musicDir)){;
-    fs.mkdirSync(musicDir)}
+if(!argv['a'] && !argv['v']){ //If no Format flag is found execute
+    console.log('Please include a Format flag! (-a for audio and -v for video)') // Output if the user forgot to add a format flag
+}
 
-setTimeout(function mainApp() {
-    if(!argv['v']){
-        console.log(vidTitle)
+
+if(argv['a'] && !argv['l']){//Audio only
+    let URL = "https://www.youtube.com/watch?v=" + argv['_']; //The Input from Command line becomes the URL
+    ytdl.getInfo(URL ,function(err, info) {
+        let vidTitle = info.title;
+        let escTitle = vidTitle.replace(/([/,(, ,)])+/g,"")
+       if(argv['f']){
+        ytdl.getInfo(URL ,function(err, info) {
+            let vidTitle = info.title;
+            let escTitle = vidTitle.replace(/([/,(, ,)])+/g,"")
+            ytdl(URL,{filter:'audioonly'}).pipe(fs.createWriteStream(`./${audioDir}/${(escTitle)}.mp3`)).on('finish', function() {
+                console.log(`Downloaded - ${vidTitle}`)
+            })
+            if (err) throw err;
+        });
+       }else{
+        let stream = ytdl(URL);
+        var proc = new ffmpeg({source: stream});
+    
+        proc.setFfmpegPath("C:/ffmpeg/bin/ffmpeg.exe");
+        proc.withAudioCodec('libmp3lame')
+            .toFormat('mp3')
+            .output(`./${audioDir}/${escTitle}.mp3`)
+            .run();
+        proc.on('end', function() {
+            console.log(`Downloaded - ${vidTitle}`);
+        });
+       }
+
+        if (err) throw err;
+    });
+}
+
+
+if(argv['v'] && !argv['l']){//Video only
+    let URL = "https://www.youtube.com/watch?v=" + argv['_']; //The Input from Command line becomes the URL
+    ytdl.getInfo(URL ,function(err, info) {
+        let vidTitle = info.title;
         let escTitle = vidTitle.replace(/([/,(, ,)])+/g,"")
         ytdl(URL).pipe(fs.createWriteStream(`./${videoDir}/${(escTitle)}.mp4`)).on('finish', function() {
-            try {
-                var process = new ffmpeg(`./${videoDir}/${escTitle}.mp4`);
-                    process.then(function (video) {
-                    // Callback mode
-                    console.log('Extracting audio to mp3 file.')
-                    video.fnExtractSoundToMP3(`./${musicDir}/${escTitle}.mp3`, function (error, file) {
-                    if (!error)
-                        console.log('Audio file: ' + file);
-                        console.log('Done - Completed without error')
-                    });
-                }, function (err) {
-                    console.log('Error: ' + err);
-                });
-            } catch (e) {
-                console.log(e.code);
-                console.log(e.msg);
-            }   
-        });
+            console.log(`Downloaded - ${vidTitle}`)
+        })
+        if (err) throw err;
+    });
+
+}
+
+
+if(argv['a'] && argv['l']){//Audio List
+    let text;
+    let list;
+
+    let file = `./${argv['_']}` //Get the file name from the Process arguments 
+    if(!fs.existsSync(file)){ // If it does not exists - execute below
+        console.log('No list file found') // Output if no file is found
     }else{
-        console.log(vidTitle)
-        let escTitle = vidTitle.replace(/([/,(, ,)])+/g,"")
-        ytdl(URL).pipe(fs.createWriteStream(`./${videoDir}/${escTitle}.mp4`));
+        text = fs.readFileSync(file, "utf-8"); // Read File and set Variable
+        list = text.split(','); // Set Variable as readable array
+        if(argv['d']){ //Debugger Flag
+            console.log(`${list.length} Videos loaded`) // Get how many were in the List Array
+        }
+        let i = 0;
+        let x = 0;
+        list.forEach(function(element){ //Function checking all are visible before executing
+            if(argv['d']){//Debugger Flag
+                console.log(`(${i+1}/${list.length}) Loaded | ${element}`) //Output for loading files
+            } 
+            let URL = "https://www.youtube.com/watch?v=" + element;
+    
+            ytdl.getInfo(URL ,function(err, info) {
+                let vidTitle;
+                
+                vidTitle = info.title;
+                let escTitle = vidTitle.replace(/([/,(, ,)])+/g,"") //Escapes Characters in Video title
+                let stream = ytdl(URL); //Sets Stream source as 
+                
+                var proc = new ffmpeg({source: stream});
+                
+                //currently have ffmpeg stored directly on the server, and ffmpegLocation is the path to its location... perhaps not ideal, but what I'm currently settled on. And then sending output directly to response.
+                proc.setFfmpegPath("C:/ffmpeg/bin/ffmpeg.exe");
+                proc.withAudioCodec('libmp3lame')
+                    .toFormat('mp3')
+                    .output(`./${audioDir}/${escTitle}.mp3`)
+                    .run();
+                proc.on('end', function() {
+                    console.log(`(${x+1}/${list.length}) Downloaded | ${vidTitle}`);
+                    x++
+                });
+                if (err) throw err;
+            });
+            i++ 
+        });
     }
-},2000)
+}
+
+
+if(argv['v'] && argv['l']){//Video List
+    let text;
+    let list;
+
+    let file = `./${argv['_']}` //Get the file name from the Process arguments 
+    if(!fs.existsSync(file)){ // If it does not exists - execute below
+        console.log('No list file found') // Output if no file is found
+    }else{
+        text = fs.readFileSync(file, "utf-8"); // Read File and set Variable
+        list = text.split(','); // Set Variable as readable array
+        if(argv['d']){ //Debugger Flag
+            console.log(`${list.length} Videos loaded`) // Get how many were in the List Array
+        }
+        let i = 0;
+        let x = 0;
+        list.forEach(function(element){ //Function checking all are visible before executing
+            if(argv['d']){//Debugger Flag
+                console.log(`(${i+1}/${list.length}) Loaded | ${element}`) //Output for loading files
+            } 
+            let URL = "https://www.youtube.com/watch?v=" + element;
+    
+            ytdl.getInfo(URL ,function(err, info) {
+                let vidTitle;
+                vidTitle = info.title;
+                let escTitle = vidTitle.replace(/([/,(, ,)])+/g,"") //Escapes Characters in Video title
+                ytdl(URL).pipe(fs.createWriteStream(`./${videoDir}/${escTitle}.mp4`)).on('finish', function(){
+                    console.log(`(${x+1}/${list.length}) Downloaded | ${vidTitle}`)
+                    x++
+                })
+                if (err) throw err;
+            });
+            i++
+        });
+    }  
+}
